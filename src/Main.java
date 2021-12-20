34ae64eeb8e7341ad9ddc390a34e13407cbaa24a
Raatbeko <jumabekovradik@gmail.com>
2021-12-20 16:04:15 +0600

    HomeWork-21

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Bus bus = new Bus(30);
         bus.addPassanger(new Passangers[]{new Passangers("Raatbek1",15),new Passangers("Raatbek2",15),new Passangers("Raatbek3",15),new Passangers("Raatbek4",15)});
-//        bus.searchPassanger();
+        bus.searchPassanger();
+        bus.showPassenger();
+        bus.calculate();
+
 
 
 
--- src/bus/Bus.java
+++ src/bus/Bus.java
@@ -6,24 +6,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bus {
-    private  final Passangers[] passangers;
+    private final Passangers[] passangers;
 
     public Bus(int size) {
         this.passangers = new Passangers[size];
     }
 
-    public void addPassanger(Passangers[] passangers){
+    public void addPassanger(Passangers[] passangers) {
         for (int i = 0; i < passangers.length; i++) {
             this.passangers[i] = passangers[i];
-            System.out.println("N" +i + ":"+this.passangers[i].getName());
+            System.out.println("N" + i + ":" + this.passangers[i].getName());
         }
+
+
+    }
+
+
+    public void removePassanger(int numElem) {
+        passangers[numElem] = null;
+    }
+
+    public void showPassenger() {
         System.out.println("Do you want add new passangers?");
         String addNewPassangers = scanner.nextLine();
 
         if (addNewPassangers.equals("Yes")) {
-
-            System.out.println("choose the please: " + Arrays.toString(this.passangers) + "\nNum: ");
-            int numOfPlease = scanner.nextInt();
 
 
             System.out.println("Name: ");
@@ -32,49 +39,44 @@ public class Bus {
 
             System.out.print("Price: ");
             int priceOfRoad = scanner.nextInt();
-            this.passangers[numOfPlease] = new Passangers(name,priceOfRoad);
+            for (int i = 0; i < passangers.length; i++) {
+                if (passangers[i] == null) {
+
+                    this.passangers[i] = new Passangers(name, priceOfRoad);
+                break;
+                }
+            }
         }
 
         System.out.println(Arrays.toString(this.passangers));
 
 
-
     }
 
+    public void searchPassanger() {
+        String findPassangers = scanner.nextLine();
+        for (int i = 0; i < passangers.length; i++) {
 
-    public void removePassanger(int numElem){
-        passangers[numElem] = null;
-    }
+            if (findPassangers.equals(passangers[i].getName())) {
 
-    public void showPassenger(Passangers[] passangerse){
-        for (Passangers passanger : passangerse) {
-            System.out.println(passanger);
-        }
-
-    }
-
-    public void searchPassanger(){
-            String findPassangers = scanner.nextLine();
-            for (int i = 0; i < passangers.length; i++) {
-
-                if (findPassangers.equals(passangers[i].getName())) {
-
-                    System.out.println(passangers[i]);
-
-                }
+                System.out.println(passangers[i]);
 
             }
 
+        }
+
 
     }
 
-    public void calculate(){
+    public void calculate() {
+        System.out.println( "Sum of price: "+Passangers.sumOfPrice);
 
     }
 
-        Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+
     public void results(Passangers[] passangers) {
-        for (int i = 0; i < passangers.length-1; i++) {
+        for (int i = 0; i < passangers.length - 1; i++) {
             System.out.println(passangers[i]);
         }
         if (Passangers.count > passangers.length) {
@@ -100,7 +102,7 @@ public class Bus {
 
                 System.out.print("Price: ");
                 int priceOfRoad = scanner.nextInt();
-                passangers[numOfPlease] = new Passangers(name,priceOfRoad);
+                passangers[numOfPlease] = new Passangers(name, priceOfRoad);
             }
 
             System.out.println(Arrays.toString(passangers));
