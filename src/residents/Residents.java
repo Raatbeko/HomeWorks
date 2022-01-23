f8c3299956736e589982246ea1257f2d24041d05
Raatbeko <jumabekovradik@gmail.com>
2022-01-23 19:34:12 +0600

    exam

--- src/residents/Residents.java
+++ src/residents/Residents.java
@@ -2,10 +2,21 @@ package residents;
 
 public class Residents {
     public static void main(String[] args) {
-        System.out.println(countResidents(5));
+
     }
-    public static String countResidents(int count){
-        if (count%2 == 0)return "They're equal!";
-        else return "left street more";
+
+    public static void countResidents(int[] count) {
+        int rightResidents = 0, leftResidents = 0;
+
+        for (int i = 0; i < count.length; i++) {
+            rightResidents += count[i];
+            leftResidents += count[i + 1];
+            i++;
+        }
+        if (rightResidents > leftResidents) {
+            System.out.println("More right side");
+        } else{
+            System.out.println("More left side");
+        }
     }
 }
