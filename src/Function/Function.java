9c868aae54e781cc5c7a224a58cf7d6aa1e2316e
Dell <jumabekovradik@gmail.com>
2022-02-25 20:29:11 +0600

    exsamFourth

--- src/Function/Function.java
+++ src/Function/Function.java
@@ -1,24 +1,35 @@
 package Function;
 
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Function {
-    public Set<Character> setOfNumber(){
+    public Set<Character> setOfNumber() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Type number-> ");
         String number = scanner.next();
 
-        Set<Character> setOfNumber = new HashSet<>();
+        Set<Character> setOfNumbers = new HashSet<>();
+        number = number.toLowerCase(Locale.ROOT);
 
+        boolean controle = true;
         for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) == ','){
+            for (char j = 'a'; j < 'z'; j++) {
+                if (number.charAt(i) == j) {
+                    System.out.println("Вести только число!!");
+                    return null;
+                }
+            }
+            if (number.charAt(i) == ',') {
                 continue;
             }
-            setOfNumber.add(number.charAt(i));
+            setOfNumbers.add(number.charAt(i));
+            controle = false;
         }
-        return setOfNumber;
+        return setOfNumbers;
     }
 }
+
