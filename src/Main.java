95a8b34718a994784a8657db673ebc1eef103580
Dell <jumabekovradik@gmail.com>
2022-03-26 19:55:35 +0600

    HomeWork-52

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 
 
 public class Main {
+    /*
+    CREATE TABLE public.users (
+	user_id serial4 NOT NULL,
+	user_name varchar(100) NOT NULL,
+	e_mail varchar(100) NOT NULL,
+	"password" varchar(50) NOT NULL,
+	data_of_registration timestamp NOT NULL,
+	is_blocked bool NULL,
+	CONSTRAINT users_e_mail_key UNIQUE (e_mail),
+	CONSTRAINT users_pkey PRIMARY KEY (user_id),
+	CONSTRAINT users_user_name_key UNIQUE (user_name)
+);
+    create table user_logs(
+    id integer primary key not null,
+    user_id integer references users(user_id),
+    results varchar(50) not null
+);
+    create table address(
+    id integer primary key not null,
+    address varchar()
+);
+    create table events_type(
+    id integer primary key not null,
+    event_type varchar(100) unique not null
+);
+
+    create table roles(
+    id integer primary key not null,
+    role varchar not null unique
+);
+
+   alter table users
+   add column role integer references roles(id);
+
+   create table events(
+   id integer primary key not null,
+   user_id integer references users(user_id),
+   event_type integer references events_type(id),
+   img text not null,
+   comment text,
+   role_id integer references roles (id),
+   count_place integer not null,
+   price integer ,
+   address integer references
+);
+     */
 
 }
